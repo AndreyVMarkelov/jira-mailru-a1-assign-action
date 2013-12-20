458daddba39679dbaba397fd47f2426c60c33a18
DmitryMiroshnichenko <dmir1988@gmail.com>
2013-12-20 18:42:22 +0400

    dead plugin

---
+++ src/main/java/ru/mail/jira/plugins/common/Consts.java
@@ -0,0 +1,19 @@
+package ru.mail.jira.plugins.common;
+
+
+public interface Consts
+{
+    String PROJECT_NAME_ALLODS = "ALLODS";
+
+    String PROJECT_NAME_VACATIONS = "VCN";
+
+    /**
+     * id for custom field "Date from"
+     */
+    Long CF_VACATION_DATE_FROM_ID = Long.valueOf(10311);
+
+    /**
+     * id for custom field "Date to"
+     */
+    Long CF_VACATION_DATE_TO_ID = Long.valueOf(10312);
+}
